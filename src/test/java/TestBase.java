ef7d47b07664c71accecde2632da73c47e5d088e
catcat4er <kerimov-t90@mail.ru>
2022-02-03 00:29:25 +0400

    added systemProperties

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -18,7 +18,7 @@ public class TestBase {
         Configuration.browserSize = "1920x1080";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         String user = System.getProperty("user");
-        String password = System.getProperty("pass");
+        String password = System.getProperty("password");
         String url = System.getProperty("url");
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
